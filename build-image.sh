7234c8eb9fa6d957e4027d7e37c0144019b196e1
Jake <spacejake@gmail.com>
2020-04-17 01:58:33 +0000

    updated

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t tfv1-15-cuda9-py3-ubuntu18 .
+docker build -t tfv1-15-cuda10-0-py3-ubuntu18 .
--- run.sh
+++ run.sh
@@ -6,14 +6,15 @@ declare data=$2
 echo "Data Path: $data_path"
 
 xhost +local:root; \
-    nvidia-docker run -it --rm --shm-size=16G \
+    docker run --runtime=nvidia -it --shm-size=16G \
+    -p 5000:8888 -p 5001:6006 \
     -e DISPLAY=$DISPLAY \
     -e "QT_X11_NO_MITSHM=1" \
-    -e CUDACXX=/opt/cuda/bin/nvcc \
+    -e CUDACXX=/usr/local/cuda/bin/nvcc \
     --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v $src:/workspace/src:rw \
     -v $data:/workspace/data:rw \
-    tfv1-15-cuda9-py3-ubuntu18
+    tfv1-15-cuda10-0-py3-ubuntu18
 
 
