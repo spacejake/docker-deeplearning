9b02df298222538922f5188f60169b8f70b52672
Jake <spacejake@gmail.com>
2020-05-22 07:57:35 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ echo "Data Path: $data_path"
 
 xhost +local:root; \
     docker run --runtime=nvidia -it --shm-size=16G \
-    -p 5000:8888 -p 5001:6006 \
     -e DISPLAY=$DISPLAY \
     -e "QT_X11_NO_MITSHM=1" \
     -e CUDACXX=/usr/local/cuda/bin/nvcc \
@@ -16,5 +15,17 @@ xhost +local:root; \
     -v $src:/workspace/src:rw \
     -v $data:/workspace/data:rw \
     tfv1-15-cuda10-0-py3-ubuntu18
+
+#xhost +local:root; \
+#    docker run --runtime=nvidia -it --shm-size=16G \
+#    -p 5000:8888 -p 5001:6006 \
+#    -e DISPLAY=$DISPLAY \
+#    -e "QT_X11_NO_MITSHM=1" \
+#    -e CUDACXX=/usr/local/cuda/bin/nvcc \
+#    --privileged \
+#    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+#    -v $src:/workspace/src:rw \
+#    -v $data:/workspace/data:rw \
+#    tfv1-15-cuda10-0-py3-ubuntu18
 
 
